4fee9306dc06b43672f3109588a2cd3e54ac0e8c
RaidoPingu <suurmets@protonmail.com>
2024-01-25 10:47:02 +0200

    Everything is object

---
+++ script3.js
@@ -0,0 +1,20 @@
+let obj = {
+    name: 'Kaspar',
+    hello() {
+        console.log('Hello')
+    }
+}
+
+console.log(obj.name);
+obj.hello()
+
+let text = 'Hello World';
+console.log(text.length);
+console.log(text[2]);
+console.log(text.toUpperCase());
+let array = [ 1, 2, 3]
+array.push(4);
+console.log(array);
+console.log(console);
+console.log(Math.PI);
+
