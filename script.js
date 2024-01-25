c11f13645efd57bbf96a6bf224bdccd359bd408f
RaidoPingu <suurmets@protonmail.com>
2024-01-25 09:15:52 +0200

    Vahepealne

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('See on minu teade!');
+let num = 1;
+const pi = 3.141595;
+var num2 = 2; // Hoisting
+
+let text = 'Hello world';
+text = "Hello World";
+text = `Hollo World 
+and pi ios ${pi}`; // kui kasutada neid, siis saab teha mitmerealisi tekste ning lisada muutujaid
+console.log(text);
+
